976c6b59014dd82ee27985cdf9d5084f7a1dbe14
breadbread1984 <breadbread1984@163.com>
2024-03-08 14:41:19 +0800

    update

--- create_dataset.py
+++ create_dataset.py
@@ -19,19 +19,19 @@ def generate_dataset(data_dir, div = 'train', output = "output.csv"):
   prompt = '以下是糖尿病问题分类单项选择题，请选出正确类别。\n\n%s\n\nA. 诊断\nB. 治疗\nC. 常识\nD. 健康生活方式\nE. 流行病学\nF. 其他\n答案：'
   with open(join(data_dir, '%s.txt' % div), 'r') as f:
     csv = reader(f, delimiter = '\t')
-  samples = list()
-  for row in csv:
-    sample = {"conversations": [
-      {
-        "role": "user",
-        "content": prompt % row[0]
-      },
-      {
-        "role": "assistant",
-        "content": "ABCDEF"[int(row[1])]
-      }
-    ]}
-    samples.append(sample)
+    samples = list()
+    for row in csv:
+      sample = {"conversations": [
+        {
+          "role": "user",
+          "content": prompt % row[0]
+        },
+        {
+          "role": "assistant",
+          "content": "ABCDEF"[int(row[1])]
+        }
+      ]}
+      samples.append(sample)
   with open(output, 'w') as f:
     f.write(json.dumps(samples))
 
