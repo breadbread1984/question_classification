1fb1eb2ebbea7e00ad7d3d4d60d43df0a1076d7d
breadbread1984 <breadbread1984@163.com>
2024-03-11 13:48:03 +0800

    update

--- create_dataset.py
+++ create_dataset.py
@@ -6,24 +6,22 @@ from csv import reader, QUOTE_ALL
 import json
 
 def convert(orig, dst):
-  samples = list()
+  outf = open(dst, 'w')
   with open(orig, 'r') as inf:
     csv = reader(inf, delimiter = '\t')
     for row in csv:
-      samples.append({'text': row[0], 'label': int(row[1])})
-  with open(dst, 'w') as outf:
-    outf.write(json.dumps({'version': "0.1.0", "data": samples}, ensure_ascii = False))
+      outf.write(json.dumps({'text': row[0], 'label': int(row[1])}, ensure_ascii = False) + '\n')
+  outf.close()
 
 def load_csv(data_dir):
   convert(join(data_dir, 'train.txt'), 'train.json')
   convert(join(data_dir, 'dev.txt'), 'dev.json')
   convert(join(data_dir, 'dev.txt'), 'test.json')
-  dataset = load_dataset('csv',
+  dataset = load_dataset('json',
                          data_files = {
                            'train': 'train.json',
                            'validate': 'dev.json',
                            'test': 'test.json'},
-                         field = 'data',
                          column_names = ['text', 'label']
                         )
   return dataset
