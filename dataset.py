f88cda4abfccf2d2e8a6e94aed67843db440c187
breadbread1984 <breadbread1984@163.com>
2024-03-11 09:51:21 +0800

    update

--- dataset.py
+++ dataset.py
@@ -6,13 +6,12 @@ from csv import reader, QUOTE_ALL
 import json
 
 def convert(orig, dst):
-  samples = list()
+  outf = open(dst, 'w')
   with open(orig, 'r') as inf:
     csv = reader(inf, delimiter = '\t')
     for row in csv:
-      samples.append({'question': row[0], 'category': int(row[1])})
-  with open(dst, 'w') as outf:
-    outf.write(json.dumps(samples))
+      outf.write(json.dumps({'question': row[0], 'category': int(row[1])}, ensure_ascii = False) + '\n')
+  outf.close()
 
 def load_csv(data_dir):
   convert(join(data_dir, 'train.txt'), 'train.json')
