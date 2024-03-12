756a41ca7eadb90bde50c755789a50f878278ac1
breadbread1984 <breadbread1984@163.com>
2024-03-12 17:53:22 +0800

    update

--- download_model.py
+++ download_model.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-from huggingface_hub import snapshot_download
-
-snapshot_download(repo_id = "google-bert/bert-base-chinese")
+from huggingface_hub import login, snapshot_download
+login(token = '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')
+snapshot_download(repo_id = "meta-llama/Llama-2-7b")
 
